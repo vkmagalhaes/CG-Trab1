4f4882c1f0ddad51acfaa6578554a65e4a8924e2
Vinicius Kastrup <viniciusmkm@gmail.com>
2012-11-25 20:39:36 -0200

    Cartucho recarrega automaticamente ao acabar

--- CG_Trab1/src/Cartucho.cpp
+++ CG_Trab1/src/Cartucho.cpp
@@ -3,6 +3,7 @@
 Cartucho::Cartucho()
 {
     proximaBala = 0;
+    status = CARTUCHO_PRONTO;
 }
 
 Municao* Cartucho::getBalas(){
@@ -10,20 +11,37 @@ Municao* Cartucho::getBalas(){
 }
 
 void Cartucho::update(Player &player){
-    if ((Mouse::hit(Mouse::LEFT)|| Keyboard::hit(Keyboard::SPACE))&&(proximaBala < NUM_BALAS)){
-        balas[proximaBala].disparar(player);
-        proximaBala++;
-    }
+    if (status == CARTUCHO_PRONTO) {
+        if ((Mouse::hit(Mouse::LEFT)|| Keyboard::hit(Keyboard::SPACE))&&(proximaBala < NUM_BALAS)){
+            disparar(player);
+        }
 
-    if (Keyboard::hit(Keyboard::R)){
-        recarregar();
-    }
+        if (Keyboard::hit(Keyboard::R)){
+            startCharge();
+        }
 
-    Text::write(COLUNA_DISPLAY,LINHA1_DISPLAY,"%s %d","Balas: ",(NUM_BALAS - proximaBala));
+        Text::write(COLUNA_DISPLAY,LINHA1_DISPLAY,"%s %d","Balas: ",(NUM_BALAS - proximaBala));
+    } else {
+        Text::write(COLUNA_DISPLAY,LINHA1_DISPLAY,"Carregando...");
+
+        if (difftime(time(NULL), chargeStart) >= CHARGING_TIME) recarregar();
+    }
 }
 
+void Cartucho::disparar(Player &player) {
+    balas[proximaBala].disparar(player);
+    proximaBala++;
+
+    if (proximaBala >= NUM_BALAS) startCharge();
+}
+
+void Cartucho::startCharge() {
+    status = CARTUCHO_CARREGANDO;
+    chargeStart = time(NULL);
+}
 
 void Cartucho::recarregar(){
+    status = CARTUCHO_PRONTO;
     proximaBala = 0;
 }
 
--- CG_Trab1/src/cartucho.h
+++ CG_Trab1/src/cartucho.h
@@ -12,12 +12,16 @@ class Cartucho
     public:
         Cartucho();
         void update(Player &player);
+        void disparar(Player &player);
+        void startCharge();
         Municao* getBalas();
         void recarregar();
         void insertCenario(Scenario &cenario);
     private:
         Municao balas[NUM_BALAS];
         int proximaBala;
+        time_t chargeStart;
+        int status;
 };
 
 #endif // CARTUCHO_H
