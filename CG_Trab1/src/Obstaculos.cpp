97a96fa12e43ba91f38632d9b3307678bf8533a2
Filipe Xavier Trindade dos Santos <filipexts@gmail.com>
2012-11-25 17:10:11 -0200

    Montando uma arvore

--- CG_Trab1/src/Obstaculos.cpp
+++ CG_Trab1/src/Obstaculos.cpp
@@ -10,25 +10,28 @@ Obstaculos::Obstaculos()
     objeto2.load("media/models/plant/plant2.md2");
     objeto2.staticBody();
     objeto2.scale(2.0);
-    objeto2.position(85,0.5,80);
+    objeto2.graphic().rotate(30,0,0,1);
+    //objeto2.direction(0,0,1);
+    objeto2.position(86,1,81);
 
     objeto4.load("media/models/plant/plant2.md2");
     objeto4.staticBody();
     objeto4.scale(2.0);
-    objeto4.direction(1,1,1);
-    objeto4.position(85,0.5,82);
+    objeto4.graphic().rotate(60,0,0,1);
+    //objeto4.direction(0,0,1);
+    objeto4.position(84,1,79);
 
     objeto5.load("media/models/plant/plant2.md2");
     objeto5.staticBody();
     objeto5.scale(2.0);
-    objeto5.direction(1,1,0);
-    objeto5.position(85,0.5,78);
+    objeto5.direction(0,0,1);
+    objeto5.position(85,1,80);
 
     objeto6.load("media/models/arbusto/fern.md2");
     objeto6.staticBody();
     objeto6.enableLighting();
     objeto6.shininess(50.0);
-    //objeto6.reflectivity(1.0);
+    objeto6.reflectivity(1.0);
     //objeto6.scale(2.0);
     objeto6.position(85,0.5,70);
 
@@ -36,9 +39,10 @@ Obstaculos::Obstaculos()
     objeto3.staticBody();
     objeto3.position(80,1,90);
 
-    objeto7.load("media/models/vegetation/grass/grass.c4d");
+    objeto7.load("media/models/Trees/trunk.md2");
     objeto7.staticBody();
-    objeto7.position(80,1,100);
+    objeto7.scale(4.0);
+    objeto7.position(85,4,80);
 
 }
 
@@ -49,5 +53,5 @@ void Obstaculos::insertCenario(Scenario &cenario){
     cenario.insert(objeto4);
     cenario.insert(objeto5);
     cenario.insert(objeto6);
-    cenario.insert(objeto7);
+    //cenario.insert(objeto7);
 }
